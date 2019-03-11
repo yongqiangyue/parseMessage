1ba6f6b23325fdfb9c8f8475447d8dcfcbcd0fa0
252895542 <yueyq2017>
2019-03-11 16:50:33 +0800

    parse simple answer

--- parseTimeMessage.py
+++ parseTimeMessage.py
@@ -13,18 +13,55 @@ class ParseTimeMessage(object):
     with open("./simpleAnswer.json",'r') as load_f:
       self.load_simple_answer = json.load(load_f)
 
-  def parseTimeMessage(self):
-    s = self.msg
+  def __parseSimple(self, s, key):
     ret_obj = {}
     ret_obj['status'] = 'complete'
-    ret = re.search(self.load_keys['shit'].encode('utf-8'), s)
+    ret = re.search(self.load_keys[key].encode('utf-8'), s)
     if ret:
       ret_obj['status'] = 'failed'
-      ret_obj['error'] = self.load_simple_answer['shit'].encode('utf-8')
+      ret_obj['error'] = self.load_simple_answer[key].encode('utf-8')
       ret_json = json.dumps(ret_obj, ensure_ascii=False)
       return ret_json
+    return None
+  
+  def __parseShit(self, s):
+     # 文明用语
+    return self.__parseSimple(s, 'shit')
+
+  def __parseSimpleAnswer(self, s):
+    ret_obj = self.__parseSimple(s, 'broken')
+    if ret_obj:
+      return ret_obj
+    ret_obj = self.__parseSimple(s, 'kill')
+    if ret_obj:
+      return ret_obj
+    ret_obj = self.__parseSimple(s, 'hello')
+    if ret_obj:
+      return ret_obj
+    ret_obj = self.__parseSimple(s, 'dowhat')
+    if ret_obj:
+      return ret_obj
+    ret_obj = self.__parseSimple(s, 'good')
+    if ret_obj:
+      return ret_obj
+    ret_obj = self.__parseSimple(s, 'fool')
+    if ret_obj:
+      return ret_obj
+    return self.__parseSimple(s, 'default')
+
+  def parseTimeMessage(self):
+    s = self.msg
+    ret_obj = self.__parseShit(s)
+    if ret_obj:
+     return ret_obj
+    
+    #todo
+    return self.__parseSimpleAnswer(s)
+    ret_obj = {}
+    ret_obj['status'] = 'complete'
+    return ret_obj
 
 if __name__ == '__main__':
-  s = '傻x'
+  s = '做什么'
   ps = ParseTimeMessage(s)
   print(ps.parseTimeMessage())
