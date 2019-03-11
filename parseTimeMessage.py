34e12a3eac9f7f433297fc7ad084338c31780eb1
252895542 <yueyq2017>
2019-03-11 15:44:17 +0800

    shit

--- parseTimeMessage.py
+++ parseTimeMessage.py
@@ -1,13 +1,30 @@
 #! encoding=utf-8
-# import sys
+
+import re
+import datetime
+import json
+import sys
 
 class ParseTimeMessage(object):
   def __init__(self, msg):
     self.msg = msg
+    with open("./keys.json",'r') as load_f:
+      self.load_keys = json.load(load_f)
+    with open("./simpleAnswer.json",'r') as load_f:
+      self.load_simple_answer = json.load(load_f)
 
   def parseTimeMessage(self):
-    return self.msg
+    s = self.msg
+    ret_obj = {}
+    ret_obj['status'] = 'complete'
+    ret = re.search(self.load_keys['shit'].encode('utf-8'), s)
+    if ret:
+      ret_obj['status'] = 'failed'
+      ret_obj['error'] = self.load_simple_answer['shit'].encode('utf-8')
+      ret_json = json.dumps(ret_obj, ensure_ascii=False)
+      return ret_json
 
 if __name__ == '__main__':
-  print("time")
-
+  s = '傻x'
+  ps = ParseTimeMessage(s)
+  print(ps.parseTimeMessage())
